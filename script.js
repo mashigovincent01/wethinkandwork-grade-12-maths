85f2a3d31e7230926820cfc63aeb11d8f45fbca3
mashigovincent01 <mashigovincent407@gmail.com>
2025-01-07 10:50:31 +0200

    initial files

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle Navbar
+document.getElementById('menu-toggle').addEventListener('click', () => {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
+});
