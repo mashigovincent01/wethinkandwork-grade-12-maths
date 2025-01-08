b724eb2313806760e01db621bdc423fe1febef9b
mashigovincent01 <mashigovincent407@gmail.com>
2025-01-08 10:21:45 +0200

    add whatsapp qr code

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ document.getElementById('menu-toggle').addEventListener('click', () => {
     const navLinks = document.getElementById('nav-links');
     navLinks.classList.toggle('active');
 });
+
+document.querySelector('#whatsapp').addEventListener('click', (event)=>{
+    event.preventDefault();
+    document.querySelector("img.qr-code").classList.toggle("hide");
+})
